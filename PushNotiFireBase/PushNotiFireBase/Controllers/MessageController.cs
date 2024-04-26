8e165a94f726dca9043e377d7ba3c50b2a2415bb
DESKTOP-VKPOJ7J\84989 <quynhvv@vinatti.vn>
2024-04-26 17:31:11 +0700

    no message

---
+++ PushNotiFireBase/PushNotiFireBase/Controllers/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using FirebaseAdmin.Messaging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PushNotiFireBase.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        [Route("SendNotificationAsync")]
+        [HttpPost]
+        public async Task SendNotificationAsync(string DeviceToken, string title, string body)
+        {
+            var message = new Message()
+            {
+                Notification = new Notification
+                {
+                    Title = "456",
+                    Body = "123"
+                },
+                Token = "dD6PCBENRkuUEzWTHZNLo_:APA91bHQ6aUGXHqjmQJzNh5qebDrwUR7mrTG7fSygpWmA_ZI9YNt2OptO_ua9zWuR7kkS8L7zProiEZjmXYVdRdSc6q_ykNfY_RoLaUnRa8oNPBnWuqnx3QsI5UUyqvaH84_iMiuqn5e",
+            };
+            var messaging = FirebaseMessaging.DefaultInstance;
+            var result = await messaging.SendAsync(message);
+        }
+    }
+}
---
+++ PushNotiFireBase/PushNotiFireBase/Program.cs
@@ -0,0 +1,31 @@
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+FirebaseApp.Create(new AppOptions()
+{
+    Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "luckynumber-b69be-9ede03c3ab8d.json")),
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
